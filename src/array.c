ec2debb538e1277453c5fd32b06c5cc9c1e06267
ocsmit <osmith2@ncsu.edu>
2023-03-09 09:22:37 -0500

    Tabs -> Spaces

--- src/array.c
+++ src/array.c
@@ -28,20 +28,20 @@ typedef struct {
  */
 Array2d *Array2d_malloc(int rows, int cols) {
 
-	// Allocate all the memory needed for the struct plus the 2d array
-	Array2d *array = (Array2d*) malloc(sizeof(Array2d));
-	assert(array != NULL);
+    // Allocate all the memory needed for the struct plus the 2d array
+    Array2d *array = (Array2d*) malloc(sizeof(Array2d));
+    assert(array != NULL);
 
-	// Possible to alloc memory for array in the struct malloc?
-	double *mat = (double*) malloc(sizeof(double[cols][rows]));
-	assert(mat != NULL);
+    // Possible to alloc memory for array in the struct malloc?
+    double *mat = (double*) malloc(sizeof(double[cols][rows]));
+    assert(mat != NULL);
 
-	array->rows = rows;
-	array->cols = cols;
-	array->data = mat;
-	array->member_size = sizeof(double);
+    array->rows = rows;
+    array->cols = cols;
+    array->data = mat;
+    array->member_size = sizeof(double);
 
-	return array;
+    return array;
 }
 
 /**
@@ -50,14 +50,14 @@ Array2d *Array2d_malloc(int rows, int cols) {
  * @param      array  pointer to Array2d 
  */
 void Array2d_free(Array2d *array) {
-	// TODO: Add checks
-	free(array->data), array->data = NULL;
-	free(array);
+    // TODO: Add checks
+    free(array->data), array->data = NULL;
+    free(array);
 }
 
 
 Array2d *Array2d_copy(Array2d *array) {
-	return Array2d_malloc(array->rows, array->cols);
+    return Array2d_malloc(array->rows, array->cols);
 }
 
 /**
@@ -70,12 +70,12 @@ Array2d *Array2d_copy(Array2d *array) {
  * @return     returns either value at (row, col), or NAN
  */
 double Array2d_get(Array2d *array, int row, int col){
-	if(row < array->rows && col < array->cols) {
-		return array->data[row * array->cols + col];
-	}
-	else {
-		return NAN;
-	}
+    if(row < array->rows && col < array->cols) {
+        return array->data[row * array->cols + col];
+    }
+    else {
+        return NAN;
+    }
 }
 
 /**
@@ -87,10 +87,10 @@ double Array2d_get(Array2d *array, int row, int col){
  * @param[in]  value  value to assign
  */
 void Array2d_assign(Array2d *array, int row, int col, double value){
-	if(row < array->rows && col < array->cols) {
-		array->data[row * array->cols + col] = value;
-	} else {
-	}
+    if(row < array->rows && col < array->cols) {
+        array->data[row * array->cols + col] = value;
+    } else {
+    }
 }
 
 
@@ -100,16 +100,16 @@ void Array2d_assign(Array2d *array, int row, int col, double value){
  * @param      array  pointer to Array2d 
  */
 void Array2d_print(Array2d* array){
-	int i, j;
+    int i, j;
 
-	printf("\n ARRAY (%d x %d) \n --- \n", 
-		(int) array->rows, (int) array->cols);
-	for(i = 0; i < array->rows; i++){
-		for(j = 0; j < array->cols; j++){
-			printf(" %.2f\t", Array2d_get(array, i, j));
-		}
-		printf("\n");
-	}
+    printf("\n ARRAY (%d x %d) \n --- \n", 
+        (int) array->rows, (int) array->cols);
+    for(i = 0; i < array->rows; i++){
+        for(j = 0; j < array->cols; j++){
+            printf(" %.2f\t", Array2d_get(array, i, j));
+        }
+        printf("\n");
+    }
 }
 
 /**
@@ -122,68 +122,46 @@ void Array2d_print(Array2d* array){
  * @return     Returns 1 if axis size is the same, 0 otherwise
  */
 int Array2d_compare_axis(Array2d *arr_a, Array2d *arr_b, int axis) {
-	switch (axis) {
-	case 0:
-		if (arr_a->rows == arr_b->rows) return 1;
-		break;
-	case 1:
-		if (arr_a->cols == arr_b->cols) return 1;
-		break;
-	}
-	return 0;
+    switch (axis) {
+    case 0:
+        if (arr_a->rows == arr_b->rows) return 1;
+        break;
+    case 1:
+        if (arr_a->cols == arr_b->cols) return 1;
+        break;
+    }
+    return 0;
 }
 
 
 int Array2d_same_size(Array2d *arr_a, Array2d *arr_b) {
-	if (Array2d_compare_axis(arr_a, arr_b, 0) != 1 ||
-		Array2d_compare_axis(arr_a, arr_b, 1) != 1) {
-		return FALSE;
-	} else {
-		return TRUE;
-	}
+    if (Array2d_compare_axis(arr_a, arr_b, 0) != 1 ||
+        Array2d_compare_axis(arr_a, arr_b, 1) != 1) {
+        return FALSE;
+    } else {
+        return TRUE;
+    }
 }
 
-Array2d *Array2d_add(Array2d *arr_a, Array2d *arr_b) {
-	int rows, cols;
-
-	if (Array2d_same_size(arr_a, arr_b) == FALSE) {
-		return 0;
-	}
-
-	Array2d *arr_result = Array2d_copy(arr_a);
-	rows = arr_result->rows;
-	cols = arr_result->cols;
-
-	// Difficulty of SIMD for arm chip?
-	// Difficulty of portable SIMD?
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j ++) {
-			Array2d_assign(arr_result, i, j, 
-				Array2d_get(arr_a, i, j) + Array2d_get(arr_b, i, j));
-		}
-	}
-
-	return arr_result;
-}
 
 
 Array2d *Array2d_subtract(Array2d *arr_a, Array2d *arr_b) {
-	int rows, cols;
+    int rows, cols;
 
-	if (Array2d_same_size(arr_a, arr_b) == FALSE) {
-		return 0;
-	}
+    if (Array2d_same_size(arr_a, arr_b) == FALSE) {
+        return 0;
+    }
 
-	Array2d *arr_result = Array2d_copy(arr_a);
-	rows = arr_result->rows;
-	cols = arr_result->cols;
+    Array2d *arr_result = Array2d_copy(arr_a);
+    rows = arr_result->rows;
+    cols = arr_result->cols;
 
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j ++) {
-			Array2d_assign(arr_result, i, j, 
-				Array2d_get(arr_a, i, j) - Array2d_get(arr_b, i, j));
-		}
-	}
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j ++) {
+            Array2d_assign(arr_result, i, j, 
+                Array2d_get(arr_a, i, j) - Array2d_get(arr_b, i, j));
+        }
+    }
 
-	return arr_result;
+    return arr_result;
 }
--- src/testing.c
+++ src/testing.c
@@ -10,41 +10,41 @@
 
 int random_int(int lower, int upper)
 {
-	int num = (rand() % (upper - lower + 1)) + lower;
-	return num;
+    int num = (rand() % (upper - lower + 1)) + lower;
+    return num;
 }
 
 
 int main(int argc, char const *argv[])
 {
-	// Seed and populate array
-	srand(42);
+    // Seed and populate array
+    srand(42);
 
-	Array2d *array_a = Array2d_malloc(2, 2);
-	Array2d *array_b = Array2d_malloc(2, 2);
-	Array2d *array_c;
-	// If we want to access just the data array
-	// we can use -> double *data = array->data;
+    Array2d *array_a = Array2d_malloc(2, 2);
+    Array2d *array_b = Array2d_malloc(2, 2);
+    Array2d *array_c;
+    // If we want to access just the data array
+    // we can use -> double *data = array->data;
 
-	for (int i = 0; i < array_a->rows; i++) {
-		for (int j = 0; j < array_a->cols; j ++) {
-			Array2d_assign(array_a, i, j, random_int(1, 255));
-			Array2d_assign(array_b, i, j, random_int(1, 255));
-		}
+    for (int i = 0; i < array_a->rows; i++) {
+        for (int j = 0; j < array_a->cols; j ++) {
+            Array2d_assign(array_a, i, j, random_int(1, 255));
+            Array2d_assign(array_b, i, j, random_int(1, 255));
+        }
 
-	}
+    }
 
-	Array2d_print(array_a);
-	Array2d_print(array_b);
+    Array2d_print(array_a);
+    Array2d_print(array_b);
 
-	array_c = Array2d_add(array_a, array_b);
-	Array2d_print(array_c);
+    array_c = Array2d_add(array_a, array_b);
+    Array2d_print(array_c);
 
-	array_c = Array2d_subtract(array_a, array_b);
-	Array2d_print(array_c);
+    array_c = Array2d_subtract(array_a, array_b);
+    Array2d_print(array_c);
 
-	Array2d_free(array_b)	;
+    Array2d_free(array_b)   ;
 
-	return 0;
+    return 0;
 }
 
